ea6ee3467ce5f3bd7f51ae15f1ad24b53ec0b1fd
Jake Dillon <jake.dillon168@gmail.com>
2018-10-30 14:43:30 -0500

    started UI

---
+++ TaskManagerIOS/Utilities/TaskViewTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  TaskViewTableCell.swift
+//  TaskManagerIOS
+//
+//  Created by Jake Dillon on 10/30/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import Foundation
+
+
+class GameTableViewCell: UITableViewCell {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
